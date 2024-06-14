7103e1890509ae2e0b67080fdef5ed45dd6024d3
penspanic <penspanic@gmail.com>
2024-06-14 11:33:33 +0900

    ts deltaTime가 계속 0인 문제 수정

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import { main } from "./main";
 import "./style.css";
 
 new p5((p5Instance) => {
-  const p = p5Instance as unknown as p5;
-  window.p = p;
-  let deltaTime = 0;
-  let lastMills = 0;
-  window.deltaTime = deltaTime;
-  window.updateDeltaTime = () => {
-    const currentMills = Date.now();
-    deltaTime = (currentMills - lastMills) / 1000;
-    lastMills = currentMills;
-  };
-  main(p);
+    const p = p5Instance as unknown as p5;
+    window.p = p;
+    //let deltaTime = 0;
+    let lastMills = 0;
+    window.deltaTime = 0;
+    window.updateDeltaTime = () => {
+        const currentMills = Date.now();
+        window.deltaTime = (currentMills - lastMills) / 1000;
+        lastMills = currentMills;
+        //console.log('deltatime: ' + deltaTime)
+    };
+    main(p);
 }, document.getElementById("app")!);
